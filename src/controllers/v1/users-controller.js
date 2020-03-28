b3cbfd859273b44901ee9c0abbfa019ba5f96fb9
Tony <antoniosalinasolivares@hotmail.com>
2020-03-28 00:46:31 -0600

    async implemebtation and encryption

--- src/controllers/v1/users-controller.js
+++ src/controllers/v1/users-controller.js
@@ -1,8 +1,78 @@
-const createUser = (request, response) =>{
-    response.send({status:'ok', message:'user created'});
-    console.log('created user');
-};
+const bcrypt = require('bcrypt');
 
+const createUser = async (request, response) =>{
+    
+    /**
+     * Con async y await
+     * ** Para esto, la funcion createUser debe ser asyncrona, 
+     * ** es decir debe ser declarada:
+     * ** const createUser = async(request, response) => {
+     * ** //codigo
+     * ** }
+     * ** para los otros metodos no es necesario usar async
+     * ** puede ser removido
+     * Notese que por definicion las funciones await no arrojan errores
+     * Pero si pueden manejarse errores de ejecucion 
+     * en un 
+     * ** try{
+     *      bcrypt.hash(params)
+     * ** }catch(e){
+     * **   handling(e)
+     * ** }
+     */
+
+    const hash = await bcrypt.hash(request.body.password, 15)
+    console.log('Contraseña cifrada', hash);
+    response.send({
+        status: 'Success', 
+        message : 'Usuario Guardado'
+    }); 
+
+    /**
+     * Con callbacks
+     
+    
+    bcrypt.hash(request.body.password, 15 , (err, hash) => {
+        if(!err){
+            // En esta parte iria nuestra consulta a la base de datos
+            console.log('Contraseña cifrada', hash);
+            response.send({
+                status: 'Success', 
+                message : 'Usuario Guardado'
+            }); 
+        } else {
+            console.log('Unable to encrypt the password')
+            response.status(505).send({
+                status : 'Error', 
+                message : 'invalida password'
+            })
+        }
+    });
+    */
+    /**
+     * Con Promesas
+    
+
+    bcrypt.hash(request.body.password, 15)
+    .then((hash)=>{
+        // En esta parte iria nuestra consulta a la base de datos
+        console.log('Contraseña cifrada', hash);
+        response.send({
+            status: 'Success', 
+            message : 'Usuario Guardado'
+        }); 
+    })
+    .catch((err)=>{
+        console.log('Unable to encrypt the password')
+        response.status(505).send({
+            status : 'Error', 
+            message : 'invalida password'
+        })
+    })  
+    */
+
+
+};
 const deleteUser = (request, response) => {
 
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const routesv1 = require('./routes/v1');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.json())
 
 routesv1(app);
 
